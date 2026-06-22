549a6b3ebd3d93df2ce38bbfef669f27b43a53e2
Johannes Riedel <>
2026-06-22 16:58:45 +0200

    adding domcontentloaded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
-// A simple animation to test the core library
-gsap.to(".test-box", { 
-    x: 300, 
-    rotation: 360, 
-    duration: 2, 
-    ease: "bounce.out" 
+// This waits until the browser has fully loaded the HTML and GSAP
+window.addEventListener("DOMContentLoaded", () => {
+  
+    // Register your plugins
+    gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+  
+    // Run your test animation
+    gsap.to(".test-box", { 
+      x: 300, 
+      rotation: 360, 
+      duration: 2, 
+      ease: "bounce.out" 
+    });
+  
   });
