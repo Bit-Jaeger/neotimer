ecd52623efa6ed6811c10076f00a9805bddba108
Johannes Riedel <>
2026-06-23 10:05:18 +0200

    loading bar on top on startup v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 // This waits until the browser has fully loaded the HTML and GSAP
 window.addEventListener("DOMContentLoaded", () => {
   
+
     // Register your plugins
     gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
   
     // Run your test animation
     gsap.to(".test-box", { 
-      x: 300, 
-      rotation: 360, 
-      duration: 2, 
-      ease: "bounce.out" 
+      x: window.screen.width, 
+      duration: 10, 
+      ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })"
     });
   
   });
