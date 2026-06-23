eb5c4190ccdf0c7c98b8164d41b98b2344c72b3e
Johannes Riedel <>
2026-06-23 12:16:46 +0200

    trying out flex-direction: row; to format load-bar corretly up top

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
   
     // Run your test animation
     gsap.to(".test-box", { 
-      x: window.screen.width, 
+      x: -window.screen.width, 
       duration: 10, 
       ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })"
     });
