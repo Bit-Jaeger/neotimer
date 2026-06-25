71275740bb42d32673b7765b0d3d98cd420e6268
Johannes Riedel <>
2026-06-25 19:16:58 +0200

    align-self: center for interface-container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
 // This waits until the browser has fully loaded the HTML and GSAP
 window.addEventListener("DOMContentLoaded", () => {
   
-
     // Register your plugins
     gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
-  
-    // Run your test animation
-    gsap.to(".load-bar", { 
-      x: -window.screen.width, 
-      duration: 5, 
-      ease: "steps(12)"
-    });
-  
+
+    let load-bar-tl = gsap.timeline();
+
+    load-bar-tl.to(".load-bar", { 
+        x: -window.screen.width, 
+        duration: 5, 
+        ease: "steps(12)"
+      });
   });
