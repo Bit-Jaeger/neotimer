b98eca387c11013c21dba66e8d9bbbcad477415c
Johannes Riedel <>
2026-06-30 11:55:20 +0200

    testing interface flicker prototype v0.1; changing ease of load-bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
     gsap.to(".load-bar", {
         duration: 5,
         //important to have this in one line -> so it stays a string. That was the bug
-        ease: "elastic.out(1,0.3)",
+        ease: "rough({template:none.out,strength: 1,points:20,taper:none,randomize:true,clamp:false})",
         x: -window_width 
     });
 
@@ -28,5 +28,36 @@ window.addEventListener("DOMContentLoaded", () => {
         console.log("window size change to x:",window_width);
         console.log("and y:", window_height);
     }
+
+//Create Timeline to animate Boot sequence
+let tl = gsap.TimeLine();
+
+tl.to("interface-container", {opacity:1, delay:0.2})
+.add(interfaceFlicker(4,1))
+.add(interfaceNontransparent)
+.to("interface-container")
+
+
+function interfaceFlicker(duration, startFreq){
+    let interface = document.getElementById("interface-container")
+
+    interfaceTransparent(interface);
+
+
+    interfaceNontransparent(interface);
+    
+}
+
+
+function interfaceTransparent(interfaceEl){
+    interfaceEl.style.opacity="0.0";
+ }
+function interfaceNontransparent(interfaceEl){
+    interfaceEl.style.opacity="1.0";
+}
+
+
+
+
 
 });
